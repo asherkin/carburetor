a833e34ae2d454a22809ed81066f237b9296b87c
Asher Baker <asherkin@limetech.io>
2017-09-10 00:05:45 +0000

    Include stack contents and registers in output

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 
 #include <json.hpp>
 
+#include <base64_default_rfc4648.hpp>
+
 #include <mysql++/mysql++.h>
 
 #include <google_breakpad/processor/minidump.h>
@@ -14,6 +16,7 @@
 #include <google_breakpad/processor/process_state.h>
 #include <google_breakpad/processor/call_stack.h>
 #include <google_breakpad/processor/stack_frame.h>
+#include <google_breakpad/processor/stack_frame_cpu.h>
 #include <google_breakpad/processor/code_module.h>
 #include <google_breakpad/processor/system_info.h>
 #include <processor/logging.h>
@@ -35,9 +38,12 @@ using google_breakpad::ProcessState;
 using google_breakpad::ProcessResult;
 using google_breakpad::CallStack;
 using google_breakpad::StackFrame;
+using google_breakpad::StackFrameX86;
+using google_breakpad::StackFrameAMD64;
 using google_breakpad::PathnameStripper;
 using google_breakpad::CodeModule;
 using google_breakpad::SystemInfo;
+using google_breakpad::MemoryRegion;
 using google_breakpad::MinidumpThreadList;
 using google_breakpad::MinidumpMemoryList;
 
@@ -65,6 +71,56 @@ string RenderFrame(const StackFrame *frame) {
   }
   out << std::dec;
   return out.str();
+}
+
+json GetStackContents(const StackFrame *frame, const StackFrame *prev_frame, const string &cpu, const MemoryRegion *memory) {
+  // Find stack range.
+  int word_length = 0;
+  uint64_t stack_begin = 0, stack_end = 0;
+  if (cpu == "x86") {
+    word_length = 4;
+    const StackFrameX86 *frame_x86 = static_cast<const StackFrameX86*>(frame);
+    const StackFrameX86 *prev_frame_x86 = static_cast<const StackFrameX86*>(prev_frame);
+    if ((frame_x86->context_validity & StackFrameX86::CONTEXT_VALID_ESP) &&
+        (prev_frame_x86->context_validity & StackFrameX86::CONTEXT_VALID_ESP)) {
+      stack_begin = frame_x86->context.esp;
+      stack_end = prev_frame_x86->context.esp;
+    }
+  } else if (cpu == "amd64") {
+    word_length = 8;
+    const StackFrameAMD64 *frame_amd64 = static_cast<const StackFrameAMD64*>(frame);
+    const StackFrameAMD64 *prev_frame_amd64 = static_cast<const StackFrameAMD64*>(prev_frame);
+    if ((frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_RSP) &&
+        (prev_frame_amd64->context_validity &
+         StackFrameAMD64::CONTEXT_VALID_RSP)) {
+      stack_begin = frame_amd64->context.rsp;
+      stack_end = prev_frame_amd64->context.rsp;
+    }
+  }
+
+  if (!word_length || !stack_begin || !stack_end) {
+    return false;
+  }
+
+  size_t stack_offset = stack_begin - memory->GetBase();
+  size_t stack_size = stack_end - stack_begin;
+
+  if (stack_size > memory->GetSize() || stack_offset >= (memory->GetSize() - stack_size)) {
+    return false;
+  }
+
+  string data_as_string;
+  data_as_string.reserve(stack_size);
+  for (uint64_t address = stack_begin; address < stack_end; ++address) {
+      uint8_t value = 0;
+      if (!memory->GetMemoryAtAddress(address, &value)) {
+        return false;
+      }
+
+      data_as_string.push_back(value);
+  }
+
+  return base64::encode(data_as_string);
 }
 
 json SerializeCodeModule(const CodeModule *codeModule) {
@@ -126,6 +182,83 @@ json SerializeProcessState(const ProcessState *processState, RepoSourceLineResol
         frame["url"] = url;
 
       frame["rendered"] = RenderFrame(stackFrame);
+
+      const string &cpu = processState->system_info()->cpu;
+
+      if (cpu == "x86") {
+        json registers;
+        const StackFrameX86 *frame_x86 = reinterpret_cast<const StackFrameX86*>(stackFrame);
+
+        if (frame_x86->context_validity & StackFrameX86::CONTEXT_VALID_EIP)
+          registers["eip"] = frame_x86->context.eip;
+        if (frame_x86->context_validity & StackFrameX86::CONTEXT_VALID_ESP)
+          registers["esp"] = frame_x86->context.esp;
+        if (frame_x86->context_validity & StackFrameX86::CONTEXT_VALID_EBP)
+          registers["ebp"] = frame_x86->context.ebp;
+        if (frame_x86->context_validity & StackFrameX86::CONTEXT_VALID_EBX)
+          registers["ebx"] = frame_x86->context.ebx;
+        if (frame_x86->context_validity & StackFrameX86::CONTEXT_VALID_ESI)
+          registers["esi"] = frame_x86->context.esi;
+        if (frame_x86->context_validity & StackFrameX86::CONTEXT_VALID_EDI)
+          registers["edi"] = frame_x86->context.edi;
+        if (frame_x86->context_validity == StackFrameX86::CONTEXT_VALID_ALL) {
+          registers["eax"] = frame_x86->context.eax;
+          registers["ecx"] = frame_x86->context.ecx;
+          registers["edx"] = frame_x86->context.edx;
+          registers["efl"] = frame_x86->context.eflags;
+        }
+
+        frame["registers"] = registers;
+      } else if (cpu == "amd64") {
+        json registers;
+        const StackFrameAMD64 *frame_amd64 = reinterpret_cast<const StackFrameAMD64*>(stackFrame);
+
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_RAX)
+          registers["rax"] = frame_amd64->context.rax;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_RDX)
+          registers["rdx"] = frame_amd64->context.rdx;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_RCX)
+          registers["rcx"] = frame_amd64->context.rcx;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_RBX)
+          registers["rbx"] = frame_amd64->context.rbx;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_RSI)
+          registers["rsi"] = frame_amd64->context.rsi;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_RDI)
+          registers["rdi"] = frame_amd64->context.rdi;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_RBP)
+          registers["rbp"] = frame_amd64->context.rbp;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_RSP)
+          registers["rsp"] = frame_amd64->context.rsp;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_R8)
+          registers["r8"] = frame_amd64->context.r8;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_R9)
+          registers["r9"] = frame_amd64->context.r9;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_R10)
+          registers["r10"] = frame_amd64->context.r10;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_R11)
+          registers["r11"] = frame_amd64->context.r11;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_R12)
+          registers["r12"] = frame_amd64->context.r12;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_R13)
+          registers["r13"] = frame_amd64->context.r13;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_R14)
+          registers["r14"] = frame_amd64->context.r14;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_R15)
+          registers["r15"] = frame_amd64->context.r15;
+        if (frame_amd64->context_validity & StackFrameAMD64::CONTEXT_VALID_RIP)
+          registers["rip"] = frame_amd64->context.rip;
+
+        frame["registers"] = registers;
+      }
+
+      size_t callingFrameIndex = thread.size() + 1;
+      if (callingFrameIndex < callStack->frames()->size()) {
+        size_t threadIndex = threads.size();
+        const StackFrame *callingFrame = callStack->frames()->at(callingFrameIndex);
+        const MemoryRegion *threadMemory = processState->thread_memory_regions()->at(threadIndex);
+
+        frame["stack"] = GetStackContents(stackFrame, callingFrame, cpu, threadMemory);
+      }
 
       thread.push_back(frame);
     }
